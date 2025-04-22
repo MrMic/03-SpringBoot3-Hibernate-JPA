ef3010c7f11fc5a2d97e2777bf5c26f8a537d110
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-22 16:46:43 +0200

    feat(Hibernate/JPA): Java Object w/JPA - PART 2 -

--- src/main/java/fr/michaelchlon/cruddemo/CruddemoApplication.java
+++ src/main/java/fr/michaelchlon/cruddemo/CruddemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import fr.michaelchlon.cruddemo.dao.StudentDAO;
+import fr.michaelchlon.cruddemo.entity.Student;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -13,10 +16,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("John", "Doe", "john@me.com");
+
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
 }
--- src/main/java/fr/michaelchlon/cruddemo/entity/Student.java
+++ src/main/java/fr/michaelchlon/cruddemo/entity/Student.java
@@ -1,7 +1,11 @@
 package fr.michaelchlon.cruddemo.entity;
 
-import org.springframework.data.annotation.Id;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "student")
