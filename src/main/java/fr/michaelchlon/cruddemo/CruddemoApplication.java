27910cff282924a6dc5aa171ab3469f89fd2f936
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-04-26 14:57:04 +0200

    feat(Hibernate/JPA): Primary Keys

--- src/main/java/fr/michaelchlon/cruddemo/CruddemoApplication.java
+++ src/main/java/fr/michaelchlon/cruddemo/CruddemoApplication.java
@@ -18,10 +18,29 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
+
+			createMultipleStudents(studentDAO);
 		};
 	}
 
+	// ______________________________________________________________________
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create multiple student object
+		System.out.println("Creating 3 new student objects ...");
+		Student tempStudent1 = new Student("Paul", "Doe", "paul@me.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@me.com");
+		Student tempStudent3 = new Student("Bonnie", "Roe", "bonnie@me.com");
+
+		// save the student objects
+		System.out.println("Saving the student objects ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+	}
+
+	// ______________________________________________________________________
 	private void createStudent(StudentDAO studentDAO) {
 		// create the student object
 		System.out.println("Creating new student object ...");
